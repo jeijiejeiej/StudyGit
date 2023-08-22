dde7714988d95a5a82ddc58f810b0b126866e902
jie <1624803327@qq.com>
2023-08-22 15:16:57 +0800

    1

---
+++ renlianshibie-zhou/08信息录入.py
@@ -0,0 +1,20 @@
+import cv2 
+# 摄像头
+cap=cv2.VideoCapture(0)
+falg=1
+num=1
+while(cap.isOpened()):# 检测石是否在开启状态
+    ret_flag,Vshow=cap.read()#得到每帧图像
+    cv2.imshow('Capture_Test',Vshow)#显示图像
+    k=cv2.waitKey(1) & 0xFF
+    if k==ord('s'):#按键判断:按s保存
+        cv2.imwrite("C:/Users/16248/vanli/StudyGit/renlianshibie-zhou/jm/"+str(num)+".zhou"+".jpg",Vshow)
+        print("success to save"+str(num)+'.jpg')
+        print("---------------------------")
+        num+=1
+    elif k==ord(' '):#空格退出
+        break
+# 释放摄像头
+cap.release()
+# 释放内存
+cv2.destroyAllWindows()
--- renlianshibie-zhou/duqutupian.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-img=cv.imread("C:/Users/16248/vanli/StudyGit/renlianshibie-zhou/face1.png")
-# r声明原生字符串
-cv.imshow("read_img",img)
-# 展示
-cv.waitKey(0)
-# 停留秒数
-cv.destroyAllWindows()
-# 释放内存
